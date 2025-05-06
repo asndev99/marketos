94b35470baee41c81945380863a42eeaa46ff26e
lostql <anas.imran@sandstech.org>
2025-05-06 16:00:31 +0500

    fix:_id type fix in user document

--- src/Modules/User/repository/user.repository.interface.ts
+++ src/Modules/User/repository/user.repository.interface.ts
@@ -1,7 +1,10 @@
 import { IUser } from "../interface";
+import { FilterQuery } from "mongoose";
+import { IUserDocument } from "../user.model";
 
 export interface IUserRepository {
   create(data: Partial<IUser>): Promise<IUser>;
   findByUsername(username: string): Promise<IUser | null>;
   findById(id: string): Promise<IUser | null>;
+  findOne(payload: FilterQuery<IUserDocument>): Promise<IUserDocument | null>;
 }
