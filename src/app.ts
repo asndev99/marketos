47acc313c07506861a840df6c324ef954e862977
asndev <asndev99@gmail.com>
2025-08-08 21:37:30 +0500

    health-check route in app file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { globalErrorHandler } from './Middlewares/global.errorhandler.middleware';
 // import adminRouter from "./Modules/Admin/admin.router";
 import appRoutes from './index.routing';
@@ -7,10 +7,16 @@ import { loggerMiddleware } from './Configs/logger.middleware';
 dotenv.config();
 
 export default async (app: Application) => {
+
     app.use(loggerMiddleware);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
+    app.get("/", (req: any, res: any) => {
+        return res.status(200).json({
+            message: "Server is running port 8080"
+        })
+    })
     app.use('/api', appRoutes);
     app.use(globalErrorHandler);
     return app;
