ced92ccb71d9a43ae8cbf08be487fa5285cf4ab1
lostql <anas.imran@sandstech.org>
2025-09-01 23:35:15 +0500

    refactor

--- src/Modules/Cart/dto/index.ts
+++ src/Modules/Cart/dto/index.ts
@@ -3,34 +3,33 @@ import { CartItemDto } from '../interface';
 export const getCartItemsDto = (data: any): CartItemDto[] => {
     return data.length
         ? data.map((item: any) => {
-            const isDiscounted = item.productId.status === "DISCOUNTED";
+              const isDiscounted = item.productId.status === 'DISCOUNTED';
 
-            const originalPricePerItem = item.productId.price;
-            const discountedPricePerItem = isDiscounted
-                ? item.productId.discountedPrice!
-                : item.productId.price;
+              const originalPricePerItem = item.productId.price;
+              const discountedPricePerItem = isDiscounted
+                  ? item.productId.discountedPrice!
+                  : item.productId.price;
 
-            const actualPrice = originalPricePerItem * item.qty;
-            const priceAfterDiscount = discountedPricePerItem * item.qty;
+              const actualPrice = originalPricePerItem * item.qty;
+              const priceAfterDiscount = discountedPricePerItem * item.qty;
 
-            const discountPercentage = isDiscounted
-                ? ((actualPrice - priceAfterDiscount) / actualPrice) * 100
-                : 0;
+              const discountPercentage = isDiscounted
+                  ? ((actualPrice - priceAfterDiscount) / actualPrice) * 100
+                  : 0;
 
-            return {
-                cartId: item._id,
-                qty: item.qty,
-                actualEachPrice: originalPricePerItem,
-                eachPriceAfterDiscount: discountedPricePerItem,
-                actualPrice,
-                priceAfterDiscount,
-                productId: item.productId._id,
-                isDiscounted,
-                discountPercentage: parseFloat(discountPercentage.toFixed(2)),
-                images: item.productId.images.map((img: any) => img?.image),
-            };
-        })
+              return {
+                  cartId: item._id,
+                  qty: item.qty,
+                  actualEachPrice: originalPricePerItem,
+                  eachPriceAfterDiscount: discountedPricePerItem,
+                  actualPrice,
+                  priceAfterDiscount,
+                  productId: item.productId._id,
+                  productName: item.producId.name || 'No Name',
+                  isDiscounted,
+                  discountPercentage: parseFloat(discountPercentage.toFixed(2)),
+                  images: item.productId.images.map((img: any) => img?.image),
+              };
+          })
         : [];
 };
-
-
