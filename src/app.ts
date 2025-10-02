bec8a0c75d55ba140faac855d9c82ea3685aa171
SyedSaad-07 <syed.07saad@gmail.com>
2025-10-03 02:14:47 +0500

    update cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { globalErrorHandler } from './Middlewares/global.errorhandler.middleware';
-// import adminRouter from "./Modules/Admin/admin.router";
 import appRoutes from './index.routing';
 import dotenv from 'dotenv';
 import { loggerMiddleware } from './Configs/logger.middleware';
 dotenv.config();
-import cors, { CorsOptions } from 'cors';
+import cors from 'cors';
 
 export default async (app: Application) => {
 
-    const corsOptions: CorsOptions = {
-        origin: '*',
-    };
-    app.use(cors(corsOptions));
+    // const corsOptions: CorsOptions = {
+    //     origin: '*',
+    // };
+    // app.use(cors(corsOptions));
+    app.use(cors());
     app.use(loggerMiddleware);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
