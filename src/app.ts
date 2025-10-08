e5e6021a474fe45bb909ceef6f25fbf3e3ac3e8a
asndev <asndev99@gmail.com>
2025-10-08 13:28:10 +0500

    cors policy update

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,35 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Request, Response, Application } from 'express';
 import { globalErrorHandler } from './Middlewares/global.errorhandler.middleware';
 import appRoutes from './index.routing';
 import dotenv from 'dotenv';
 import { loggerMiddleware } from './Configs/logger.middleware';
-dotenv.config();
 import cors from 'cors';
+
+dotenv.config();
 
 export default async (app: Application) => {
 
-    // const corsOptions: CorsOptions = {
-    //     origin: '*',
-    // };
-    // app.use(cors(corsOptions));
-    app.use(cors(
-        {
-            origin: "*",
-            methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        }
-    ));
+    app.use(cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }));
+
+    app.options('*', cors());
+
     app.use(loggerMiddleware);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-    app.get('/', (req: any, res: any) => {
-        return res.status(200).json({
+    app.get('/', (req: Request, res: Response) => {
+        res.status(200).json({
             message: 'Server is running port 8080',
         });
     });
+
     app.use('/api', appRoutes);
+
     app.use(globalErrorHandler);
+
     return app;
 };
