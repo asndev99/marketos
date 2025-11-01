3cb6d8984eb711e50f1962a9c801a0624830b964
SyedSaad-07 <syed.07saad@gmail.com>
2025-11-01 21:04:26 +0500

    add category key in product response

--- src/Modules/Product/dto/index.ts
+++ src/Modules/Product/dto/index.ts
@@ -1,18 +1,22 @@
-import { IProductPopulatedDocument } from "../interface";
+import { IProductPopulatedDocument } from '../interface';
+import { categoryMap } from '../../../Common/constants';
 
 // Discount Percentage = (Discount Amount / Original Price) * 100
 export const discountedProductsDto = (data: any[]) => {
     return data.length
-        ? data.map((item:IProductPopulatedDocument) => {
-            return {
-                ...item,
-                discountPercentage: (item.discountedPrice / item.price) * 100,
-                images: item.images.length ? item.images.map((i: any) => {
-                    return {
-                        productUrl:i.image
-                    }   
-                }) : []
-              }
-        })
+        ? data.map((item: IProductPopulatedDocument) => {
+              return {
+                  ...item,
+                  discountPercentage: (item.discountedPrice / item.price) * 100,
+                  categoryKey: Object.keys(categoryMap).find((key) => categoryMap[key] === item?.category),
+                  images: item.images.length
+                      ? item.images.map((i: any) => {
+                            return {
+                                productUrl: i.image,
+                            };
+                        })
+                      : [],
+              };
+          })
         : [];
 };
